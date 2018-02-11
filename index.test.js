7f341494e89b487b980f28c92243137bd74f70ec
Ben Pardo <benpardo@bens-mbp-4.lan>
2018-02-10 18:49:28 -0700

    Tests subtraction.

--- index.test.js
+++ index.test.js
@@ -5,4 +5,8 @@ test("Equals four when adding two and two", function () {
         type: "SUM",
         number: 2
     })).toBe(2);
+    expect(sum(0, {
+        type: "SUBTRACT",
+        number: 3
+    })).toBe(-3);
 });
