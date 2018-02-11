29e173defdeb0832a5e144ef6eaa30ac7a0cf154
Kristen Coates <coates.kristen@gmail.com>
2018-02-10 19:01:05 -0700

    Subtracts from sum.

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ module.exports.sum = function(state = 0, action) {
     if (action.type === "SUM") {
         return state + action.number;
     }
+    if (action.type === "SUBTRACT"){
+        return state - action.number;
+    }
     return state;
 };
